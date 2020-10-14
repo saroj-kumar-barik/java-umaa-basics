8b26728b39fed62198f92b1ccb092cd78ca7b242
SAROJ KUMAR BARIK <71244681+saroj-kumar-barik@users.noreply.github.com>
2020-10-14 09:29:49 +0530

    activirty updated

--- src/main/java/org/activity/com/pkg2/B.java
+++ src/main/java/org/activity/com/pkg2/B.java
@@ -3,7 +3,7 @@ import org.activity.com.pkg1.*;
 public class B {
     public static void main(String[] args) {
         A a = new A();
-//        System.out.println(a.i);
+//        System.out.println(a.i); // private access nodifiers not accessible
         System.out.println(a.getI());
         System.out.println(a.getK());
         System.out.println(a.j);
