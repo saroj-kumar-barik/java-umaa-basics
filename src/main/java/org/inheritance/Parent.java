3e9c6b9797eea525b7e4098ada5aad03eeae67f0
saroj-kumar-barik <sarojkumarbarik1143@gmail.com>
2020-10-15 10:38:15 +0530

    inheritance basics

---
+++ src/main/java/org/inheritance/Parent.java
@@ -0,0 +1,13 @@
+package org.inheritance;
+
+public class Parent {
+    public int i;
+
+    public Parent() {
+        System.out.println("default Constructor inside parent ");
+    }
+
+    public void m1() {
+        System.out.println("A method inside parent m1()");
+    }
+}
